e8c946fda9daa3455462b90f12acb56df2fe052f
lonixoon <soinov.alexey@yandex.ru>
2017-03-16 21:01:31 +0700

    начал js popup окна

---
+++ js/popup.js
@@ -0,0 +1,4 @@
+var btn_feedback = document.querySelector(".map__address__feedback");
+var modal_feedback = document.querySelector("modal-feedback");
+var overlay = document.querySelector(".overlay");
+var close = document.querySelector(".close");
