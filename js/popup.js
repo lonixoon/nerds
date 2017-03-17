6e32a38d62a2aafbfff65c2d7cef59b1dd5b9798
lonixoon <soinov.alexey@yandex.ru>
2017-03-17 12:49:14 +0700

    Попап окно на index.html готово

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,18 @@
-var btn_feedback = document.querySelector(".map__address__feedback");
-var modal_feedback = document.querySelector("modal-feedback");
+var feedback_btn = document.querySelector(".map__address__feedback");
+var feedback_modal = document.querySelector(".modal-feedback");
 var overlay = document.querySelector(".overlay");
 var close = document.querySelector(".close");
+
+// Начало Попап окно обратной связи
+feedback_btn.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback_modal.classList.add("modal-show");
+  overlay.classList.add("modal-show");
+})
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback_modal.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
+})
+// Конец Попап окно обратной связи
